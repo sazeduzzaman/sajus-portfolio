260ce9d4fb2c244ed131ef80f9ed890b5b587b00
Szamansaju <szamansaju@gmail.com>
2025-07-26 17:47:22 +0600

    Blog Page error solved

--- src/app/allBlogs/[slug]/page.tsx
+++ src/app/allBlogs/[slug]/page.tsx
@@ -1,10 +1,7 @@
-import BlogDetails from "@/components/BlogsPage/BlogDetails/BlogDetails";
-import React from "react";
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } =await params;
-
-  return <div className="">
-    <BlogDetails slug={slug}></BlogDetails>
-  </div>;
+  const { slug } = await params;
+ 
+  return <div className="">{slug}</div>;
+  
 }
