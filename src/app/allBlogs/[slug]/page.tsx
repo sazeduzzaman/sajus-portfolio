7938193adacac05d5522ef2c6cad9ca4234ccc70
Szamansaju <szamansaju@gmail.com>
2025-07-26 17:45:39 +0600

    Blog Page error solved

--- src/app/allBlogs/[slug]/page.tsx
+++ src/app/allBlogs/[slug]/page.tsx
@@ -2,7 +2,7 @@ import BlogDetails from "@/components/BlogsPage/BlogDetails/BlogDetails";
 import React from "react";
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+  const { slug } =await params;
 
   return <div className="">
     <BlogDetails slug={slug}></BlogDetails>
