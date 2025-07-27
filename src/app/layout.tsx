0187d9f7f4b639e44c3a46a17aabfa05d5adc736
Szamansaju <szamansaju@gmail.com>
2025-07-27 18:09:02 +0600

    Layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   
   return (
-     <html lang="en" suppressHydrationWarning>
+    <html lang="en" >
       <body
         className={`${figtreeSans.variable} ${spaceGroteskSans.variable} antialiased`}
       >
